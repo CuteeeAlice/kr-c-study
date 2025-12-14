6c1ccf1182208efd4bff2c12d72c4631a5d92c61
CuteeeAlice <alice890910@gmail.com>
2025-12-14 21:59:45 +0900

    C언어 2.5일차
    배운것들을 이용할 수 있는 구조를 직접 만들어서 코딩한 예제.

---
+++ CUTECATCAFE.c
@@ -0,0 +1,85 @@
+﻿#include <stdio.h>
+
+int main() {
+
+    int shot = 500; // 샷추가 가격!
+    int price1 = 1000; // 기본가격!!
+    int price2 = 2000;
+    int price3 = 3000;
+    int price4 = 4000;
+
+    char ic[] = "ICE"; // ice약자
+    char ht[] = "HOT"; // hot약자
+    char am[] = "AMERICANO"; // ...
+    char lt[] = "LATTE"; // ...
+    char mc[] = "MOCA"; // ...
+    char st[] = "SHOT"; // ...
+     
+    int od1; // oder1
+    int od2; // oder2
+
+
+    printf("귀여운 메뉴판 >OwO<\n\n"); // 기욤둥이 메뉴판
+    printf("1. %s %s %d원!\n", ht, am, price2); // 뜨거운 아메리카노 가격
+    printf("2. %s %s %d원!\n", ht, lt, price3); // 뜨끈한 라떼 가격
+    printf("3. %s %s %d원!\n\n", ht, mc, price4); // 따끈한 모카 가격
+
+    printf("4. %s는 %d원이 추가됩니다아아!!\n", ic, price1);
+    printf("5. %s 추가는 %d원 입니다!\n\n", st, shot);
+
+    printf("주문하실 메뉴의 코드를 입력하세요. 1 ~ 3 \n", ic);
+    scanf("%d", &od1);
+
+    printf("모든 음료는 HOT이 기본이며 ICE를 원하실 경우 4 를 입력하세요.\n");
+    scanf("%d", &od2);
+
+    
+    if (od2 == 4) {
+        if (od1 == 1) {
+            printf("아이스 아메리카노를 곧 준비해드릴게요오 +ㅂ+/ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price1 + price2);
+        }
+        else if (od1 == 2) {
+            printf("아이스 카페라떼를 준비해드릴게요오 +ㅂ+/ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price1 + price3);
+        }
+        else if (od1 == 3) {
+            printf("아이스 카페모카를 준비해드릴게요오 >ㅂ</ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price1 + price4);
+        }
+        else {
+            printf("헉 그건 없는메뉴에요오 T ^T \n\n");
+        }
+        }
+
+    if (od2 != 4) {
+        if (od1 == 1) {
+            printf("따뜻한 아메리카노를 곧 준비해드릴게요오 >ㅂ</ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price2);
+        }
+        else if (od1 == 2) {
+            printf("따뜻한 카페라떼를 준비해드릴게요오 >ㅂ</ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price3);
+        }
+        else if (od1 == 3) {
+            printf("따뜻한 카페모카를 준비해드릴게요오 >ㅂ</ \n\n");
+            printf("총 금액은 %d원 입니다아아 >ㅂ<\n\n", price4);
+        }
+        else {
+            printf("헉 그건 없는메뉴에요오 T ^T \n\n");
+        }
+        }
+
+    printf("\n\n         >OwO< 고양이 카페\n\n\n\n\n\n"); // 귀여운 고냥이 카페에용!
+
+    return 0;
+}
+
+// 배웠던 개념들로만 만들어본거라 쫌 비효율적일수도 있어요...
+// 이건 이 코드쓰면 되는데? = 몰라서 안쓴거에요...ㅠㅠ
+// 왜 그렇게 했어요? = 어떻게 하는지 몰라서요
+// 작성자님이 알고 있는걸로도 충분히 구현가능한데? = 복잡해서 그렇게 생각못해봤어요
+// if에 if에 if에 else에 다시 if에.. 으아아악!!!
+// 프로그램언어를 아무것도 몰랐던 한 소녀가 GPT에게 2.5일간 배운것들 기록
+// 깃허브 처음이라 어떻게 쓰는지 잘몰라요...
+// 감삼다!!
